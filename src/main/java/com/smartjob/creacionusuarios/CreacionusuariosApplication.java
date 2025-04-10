7a16ef43cca156b045c43971dd03f3d743915e9e
JorgeValencia <jor.valencia@hotmail.es>
2025-04-10 16:02:42 -0400

    initial structure
    initial structure

---
+++ src/main/java/com/smartjob/creacionusuarios/CreacionusuariosApplication.java
@@ -0,0 +1,13 @@
+package com.smartjob.creacionusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreacionusuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreacionusuariosApplication.class, args);
+	}
+
+}
